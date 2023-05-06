ddd0c082db50fcc3989a46c3077691ae4a0ef155
yuhorn <makkachin3@gmail.com>
2023-05-06 14:32:15 +0900

    アクションとルーティングの設定

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,6 +2,12 @@ class AlbumsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+  end
+
   private
   def album_params
     params.require(:album).permit(:title, :category, :album_image).merge(user_id: current_user.id)
