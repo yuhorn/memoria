1785729fec2d0a705fc3a2dde69ee15b0ce91346
yuhorn <makkachin3@gmail.com>
2023-05-06 14:12:00 +0900

    ストロングパラメーター設定

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,4 +1,9 @@
 class AlbumsController < ApplicationController
   def index
   end
+
+  private
+  def album_params
+    params.require(:album).permit(:title, :category, :album_image).merge(:user_id: current_user.id)
+  end
 end
