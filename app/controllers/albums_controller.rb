d482e1f7a1cae1940a4a6ea28b3da28214f52ddd
yuhorn <makkachin3@gmail.com>
2023-05-06 14:12:52 +0900

    ストロングパラメーター修正

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -4,6 +4,6 @@ class AlbumsController < ApplicationController
 
   private
   def album_params
-    params.require(:album).permit(:title, :category, :album_image).merge(:user_id: current_user.id)
+    params.require(:album).permit(:title, :category, :album_image).merge(user_id: current_user.id)
   end
 end
