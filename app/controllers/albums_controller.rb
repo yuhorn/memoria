fb27a0814d582b817797f37b3bf483e3f7b936e7
yuhorn <makkachin3@gmail.com>
2023-05-09 23:05:35 +0900

    showアクション追加

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -16,6 +16,9 @@ class AlbumsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
   def album_params
     params.require(:album).permit(:title, :category, :album_image).merge(user_id: current_user.id)
