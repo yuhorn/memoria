0c41068693f24003e2a589ae44401873de809079
yuhorn <makkachin3@gmail.com>
2023-05-19 11:46:21 +0900

    モデル（クラス）、バリデーション設定

--- db/migrate/20230506044607_create_albums.rb
+++ db/migrate/20230506044607_create_albums.rb
@@ -1,9 +1,9 @@
 class CreateAlbums < ActiveRecord::Migration[6.0]
   def change
     create_table :albums do |t|
-      t.string     :title,    null: false
-      t.integer    :category, null: false
-      t.references :user,     null: false, foreign_key: true
+      t.string     :title,       null: false
+      t.integer    :category_id, null: false
+      t.references :user,        null: false, foreign_key: true
       t.timestamps
     end
   end
