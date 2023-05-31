ff1ef734edc550f5021a70b7e1057ac755a75c5e
yuhorn <makkachin3@gmail.com>
2023-05-31 21:33:26 +0900

    example作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+    end
+    it 'emailが空では登録できない' do
+    end
+    it 'passwordが空では登録できない' do
+    end
+    it '確認用passwordが空では登録できない' do
+    end
+  end
 end
