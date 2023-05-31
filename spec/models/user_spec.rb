c9e2cd537930ce4bdf0614071e0d0f8cbec38820
yuhorn <makkachin3@gmail.com>
2023-05-31 21:38:02 +0900

    FactoryBot設定

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,16 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
+      user = FactoryBot.build(:user)
     end
     it 'emailが空では登録できない' do
+      user = FactoryBot.build(:user)
     end
     it 'passwordが空では登録できない' do
+      user = FactoryBot.build(:user)
     end
     it '確認用passwordが空では登録できない' do
+      user = FactoryBot.build(:user)
     end
   end
 end
