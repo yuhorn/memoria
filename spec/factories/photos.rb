25bb37b0a51a38da2569eba5841d64e496a9d1fa
yuhorn <makkachin3@gmail.com>
2023-06-19 15:58:17 +0900

    FactoryBot設定

---
+++ spec/factories/photos.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :photo do
+    memo  {'test'}
+    date  {'2023-06-20'}
+    image {Rack::Test::UploadedFile.new("public/images/test_image.png", "image/png")}
+    association :album
+  end
+end
