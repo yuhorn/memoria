d51b2495516f8f075018deadfb17c96d0a8b268d
yuhorn <makkachin3@gmail.com>
2023-06-20 23:02:49 +0900

    単体テストコード実装

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -14,5 +14,20 @@ RSpec.describe Photo, type: :model do
       @photo.valid?
       expect(@photo.errors.full_messages).to include("Memo can't be blank")
     end
+    it 'dateが空では作成できない' do
+      @photo.date = ''
+      @photo.valid?
+      expect(@photo.errors.full_messages).to include("Date can't be blank")
+    end
+    it 'imageが紐づいていないと作成できない' do
+      @photo.image = nil
+      @photo.valid?
+      expect(@photo.errors.full_messages).to include("Image can't be blank")
+    end
+    it 'memoが10文字以下でないと作成できない' do
+      @photo.memo = 'あああああああああああ'
+      @photo.valid?
+      expect(@photo.errors.full_messages).to include("Memo is too long (maximum is 10 characters)")
+    end
   end
 end
